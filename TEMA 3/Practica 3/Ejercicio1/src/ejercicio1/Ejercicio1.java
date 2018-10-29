f4310f0936d56d2d7ca69fb19fc91460ed8b44fd
Sergio Zulueta <sergio.zulueta@ikasle.egibide.org>
2018-10-29 22:45:09 +0100

    Ejercicio1 practica 3

--- TEMA 3/Practica 3/Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ TEMA 3/Practica 3/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -13,23 +13,80 @@ import javax.swing.JOptionPane;
  */
 public class Ejercicio1 {
 
+    static int horas = 0;
+    static char estado;
+    static char estudios;
+    static int sueldo = 0;
+    static int horasExtra = 0;
+    static int horasNormales = 0;
+    static int respuesta;
+
     public static void main(String[] args) {
         // TODO code application logic here
-        int horas;
-        String estado;
-        String estudios;
-        int sueldo;
 
-        horas = Integer.parseInt(JOptionPane.showInputDialog("Introduce las horas trabajadas"));
-        estado = JOptionPane.showInputDialog("Introduce el estado civil");
-        estudios = JOptionPane.showInputDialog("Introduce los estudios");
+        do {
+            try {
+                horas = Integer.parseInt(JOptionPane.showInputDialog("Introduce las horas trabajadas"));
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
+                System.exit(horas);
+            }
+
+            try {
+                estado = JOptionPane.showInputDialog("Introduce el estado civil").charAt(0);
+                estudios = JOptionPane.showInputDialog("Introduce los estudios").charAt(0);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+
+            horasPagadas();
+            estadoCivilYEstudios();
+            
+            JOptionPane.showMessageDialog(null, sueldo);
+
+            respuesta = JOptionPane.showConfirmDialog(null, "Quieres continuar?");
+            
+        } while (respuesta == 0);
+
+        
 
     }
 
-    public static void hoasPagadas(int horas, int sueldo) {
-        if(horas <= 40){
+    public static int horasPagadas() {
+        if (horas <= 40) {
             sueldo = horas * 10;
+        } else {
+            horasExtra = (horas - 40) * 15;
+            horasNormales = horas * 10;
+            sueldo = horasNormales + horasExtra;
         }
+        return sueldo;
+    }
+
+    public static int estadoCivilYEstudios() {
+        if (estado == 'S' && estudios == 'P') {
+            sueldo = sueldo + 100;
+        }
+        if (estado == 'S' && estudios == 'M') {
+            sueldo = sueldo + 100;
+        }
+        if (estado == 'S' && estudios == 'S') {
+            sueldo = sueldo + 100;
+        }
+        if (estado == 'C' && estudios == 'S') {
+            sueldo = sueldo + 100;
+        }
+        if (estado == 'V' && estudios == 'S') {
+            sueldo = sueldo + 100;
+        }
+        if (estado == 'V' && estudios == 'P') {
+            sueldo = sueldo + 100;
+        }
+        if (estado == 'D' && estudios == 'S') {
+            sueldo = sueldo + 100;
+        }
+
+        return sueldo;
     }
 
 }
