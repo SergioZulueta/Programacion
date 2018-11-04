5b083e09feb46c14c94eeb6f0d5e5ff8cd76b19b
Sergio Zulueta <sergio.zulueta@ikasle.egibide.org>
2018-11-04 19:05:17 +0100

    Ejercicio3

--- TEMA 3/Practica 3/Ejercicio3/src/ejercicio3/Ejercicio3.java
+++ TEMA 3/Practica 3/Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -5,6 +5,8 @@
  */
 package ejercicio3;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author hp_pc
@@ -16,6 +18,78 @@ public class Ejercicio3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int opcion = 0;
+
+        opcion = Integer.parseInt(JOptionPane.showInputDialog("Que operacion deseas hacer"));
+
+        switch (opcion) {
+            case 1:
+                sumarNumero();
+                break;
+
+            case 2:
+                restarNumero();
+                break;
+
+            case 3:
+                tablaMultiplicacion();
+                break;
+
+            case 4:
+                cocienteYResto();
+                break;
+
+            case 5:
+                salirPrograma();
+                break;
+        }
+
     }
-    
+
+    public static void sumarNumero() {
+        int numero1;
+        int numero2;
+        int suma;
+        numero1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el primer numero"));
+        numero2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el segundo numero"));
+
+        suma = numero1 + numero2;
+
+        JOptionPane.showMessageDialog(null, suma);
+
+    }
+
+    public static void restarNumero() {
+        int numero1;
+        int numero2;
+        int resta;
+        numero1 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el primer numero"));
+        numero2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce el primer numero"));
+
+        resta = numero1 - numero2;
+
+        JOptionPane.showMessageDialog(null, resta);
+
+    }
+
+    public static void tablaMultiplicacion() {
+        int numero;
+        String resultado = null;
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce el primer numero"));
+
+        for (int i = 1; i <= 10; i++) {
+            resultado = resultado + "\n" + numero + " x " + i + " = " + numero * i;
+        }
+        JOptionPane.showMessageDialog(null, "Tabla del " + numero + "\n" + resultado);
+
+    }
+
+    public static void cocienteYResto() {
+
+    }
+
+    public static void salirPrograma() {
+        System.exit(0);
+    }
+
 }
