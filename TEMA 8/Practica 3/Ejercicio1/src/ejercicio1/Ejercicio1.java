c6319b2cf1ef2125abd073cc567c9f10c7625f9f
Unknown <sergio.zulueta@ikasle.egibide.org>
2019-01-25 14:11:01 +0100

    update

---
+++ TEMA 8/Practica 3/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+import Vistas.Ventana;
+
+/**
+ *
+ * @author hp_pc
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static Ventana v;
+    public static void main(String[] args) {
+        // TODO code application logic here
+        v = new Ventana();
+        v.setVisible(true);
+    }
+    
+}
